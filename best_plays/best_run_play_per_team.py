86dc35fd33a422e526ff51db259aaed99f6731d2
Luke Dillon <luke.k.dillon@gmail.com>
2022-03-22 14:57:04 -0700

    mod run play per team

--- best_plays/best_run_play_per_team.py
+++ best_plays/best_run_play_per_team.py
@@ -1,7 +1,6 @@
 # best play per team
 # most in category
 # most successful in category
-# most ran run play per team
 # most efficient run team
 # most efficient run type per team
 # most popular play per team
@@ -25,6 +24,9 @@ def get_most_in_category(runtype, filename):
         print("==")
         the_file.close()
         break
+
+
+
 
 
 run_plays = defaultdict(list)
@@ -51,6 +53,25 @@ def populate_dict(files):
         the_file.close()
     
     
+def get_team_plays(team):
+    plays = run_plays[team]
+    for p in plays:
+        ypc = int(p[3]) / int(p[2])
+        p = [ypc] + p
+        print(p)
+
+
+def get_all_team_plays():
+    print("## All Team Running Plays")
+    print("TEAM, YPC, ATTEMPTS RANK, TEAM, ATT, YARDS, PERCENT OF TOTAL PLAYS, RUN TYPE")
+    for k, plays in run_plays.items():
+        print(k)
+        for p in plays:
+            ypc = int(p[3]) / int(p[2])
+            p = [ypc] + p
+            print(p)
+        print("---")
+    
 
 
 def most_attempted_play_per_team():
@@ -63,19 +84,75 @@ def most_attempted_play_per_team():
                 great = i
         print(great)
         print("===")
+
+
+def most_efficient_run_play_per_team():
+    print(f"## Most efficient run type per team")
+    print("TEAM, YPC, ATTEMPTS RANK, TEAM, ATT, YARDS, PERCENT OF TOTAL PLAYS, RUN TYPE")
+    ypc_hash = {}
+    for k, v in run_plays.items():
+        great = []
+        great_check = 0
+        for i in v:
+            ypc = int(i[3]) / int(i[2])
+            if ypc > great_check:
+                great_check = ypc
+                great = i
+        ypc_hash[great[1]] = [great_check, great]
+        # print(f"{great[1]} : {great_check} : {great[5]}")
+        # print("===")
+    
+    ypc_hash = dict(sorted(ypc_hash.items(), reverse=True, key=lambda item: item[1]))
+    for k, v in ypc_hash.items():
+        print(f"{k} : {v}")
+
+    
+
+def most_efficient_run_team():
+    print("## Most efficient run team")
+    print("TEAM, YPC, ATT, YARDS")
+    eff_hash = {}
+    for k, v in run_plays.items():
+        att = 0
+        yds = 0
+        for i in v:
+            att += int(i[2])
+            yds += int(i[3])
+        
+        eff_hash[k] = [yds / att, att, yds]
+        # print(f"{great[1]} : {great_check} : {great[5]}")
+        # print("===")
+    
+    eff_hash = dict(sorted(eff_hash.items(), reverse=True, key=lambda item: item[1]))
+    for k, v in eff_hash.items():
+        print(f"{k} : {v}")
         
     
 
-
-print("rank, team, attempts, yards, pct of plays")
-get_most_in_category("right end", "re.csv")
-get_most_in_category("right tackle", "rt.csv")
-get_most_in_category("right guard", "rg.csv")
-get_most_in_category("center", "c.csv")
-get_most_in_category("left guard", "lg.csv")
-get_most_in_category("left tackle", "lt.csv")
-get_most_in_category("left end", "le.csv")
-print("======")
+print("# Plays and their efficiency")
+print("""Below I have compield the data for: """)
+print("- most efficient run team: ie most yards per carry (ypc)")
+print("- most efficient run type per team")
+print("- all run plays per team")
+print("<br />")
+print("Eventually I will get to the passing plays.")
+print("---")
+# 
+# print("rank, team, attempts, yards, pct of plays")
+# get_most_in_category("right end", "re.csv")
+# get_most_in_category("right tackle", "rt.csv")
+# get_most_in_category("right guard", "rg.csv")
+# get_most_in_category("center", "c.csv")
+# get_most_in_category("left guard", "lg.csv")
+# get_most_in_category("left tackle", "lt.csv")
+# get_most_in_category("left end", "le.csv")
+# print("======")
 
 populate_dict(["re.csv", "rt.csv", "rg.csv", "c.csv", "lg.csv", "lt.csv", "le.csv"])
-most_attempted_play_per_team()
+print("---")
+most_efficient_run_team()
+print("---")
+most_efficient_run_play_per_team()
+print("---")
+get_all_team_plays()
+print("---")
